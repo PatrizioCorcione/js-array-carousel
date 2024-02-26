0fa0b6e1cc48dcc0b332e3bc41a48038dda666a1
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-26 15:24:29 +0100

    [problem] function click

--- js/script.js
+++ js/script.js
@@ -6,14 +6,23 @@ const imgTot = [
   "assets/img/04.webp",
   "assets/img/05.webp"
 ]
-
+let counter = 0;
+const upJs = document.querySelector(".up")
+const downJs = document.querySelector(".down")
+console.log(counter);
 
 for (let i = 0; i < imgTot.length; i++) {
   const imgFocus = imgTot[i];
-  imgsJs.innerHTML += `"<img class = 'hide img' src=${imgFocus} alt=>"`
+  imgsJs.innerHTML += `<img class = 'hide img' src=${imgFocus} alt=>`
 }
 
 const imgComp = imgsJs.getElementsByClassName("img");
-console.log(imgComp);
 
 imgComp[0].classList.remove("hide");
+
+downJs.addEventListener('click',function(){
+  imgComp[counter].classList.add("hide")
+  counter++;
+  imgComp[counter].classList.remove("hide")
+  
+})
