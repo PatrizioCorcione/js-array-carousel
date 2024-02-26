6dda65305d2c8c2203a55e1c9b112036723848b9
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-02-26 14:19:32 +0100

    [add] struttura immagini nascoste

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-let test = Math.ceil(Math.random() * 10);
-console.log(test);
+const imgsJs = document.querySelector(".imgs")
+const imgTot = [
+  "assets/img/01.webp",
+  "assets/img/02.webp",
+  "assets/img/03.webp",
+  "assets/img/04.webp",
+  "assets/img/05.webp"
+]
+
+
+for (let i = 0; i < imgTot.length; i++) {
+  const imgFocus = imgTot[i];
+  imgsJs.innerHTML += `"<img class = 'hide img' src=${imgFocus} alt=>"`
+}
+
+const imgComp = imgsJs.getElementsByClassName("img");
+console.log(imgComp);
+
+imgComp[0].classList.remove("hide");
